556a0cb42f332225e81be9f8456b4d9035e52560
nelson alayon <nelson.alayon@gmail.com>
2026-02-25 12:52:45 -0500

    plugins.ts updated

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default ({ env }) => ({
+	'users-permissions': {
+		config: {
+			jwtSecret: env('JWT_SECRET'),
+			jwt: {
+				expiresIn: env('JWT_EXPIRES_IN', '7d'),
+			},
+		},
+	},
+});
